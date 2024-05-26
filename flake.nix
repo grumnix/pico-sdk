de1b6ac74ecd904ffcf62ffdc4e0262c9ebe3491
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:55:25 +0200

    Update to pico-sdk-1.5.1 and nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A 2D platform game featuring Tux the penguin";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,13 +17,13 @@
           pico-sdk = (pkgs.pico-sdk.overrideAttrs (o:
             rec {
               pname = "pico-sdk";
-              version = "1.4.0";
+              version = "1.5.1";
               src = pkgs.fetchFromGitHub {
                 fetchSubmodules = true;
                 owner = "raspberrypi";
                 repo = pname;
                 rev = version;
-                sha256 = "sha256:1wihm752wm3mnnrqnr7vjvrlzrhpd418jgf9i5bfajri45qrl6vs";
+                sha256 = "sha256-GY5jjJzaENL3ftuU5KpEZAmEZgyFRtLwGVg3W1e/4Ho=";
               };
               fixupPhase = ''
                 # do nothing
